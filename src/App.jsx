12def85c867bf3f7f8a44d957aa48fb1ca2bb782
Anvesha Patel <anveshap103@gmail.com>
2026-04-23 21:29:24 +0530

    Comment out unused components in App
    Comment out Navbar and HotelSearchWidget components in App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import React, { useState } from 'react';
-// import Navbar from './features/NavBar/Navbar.jsx';  <-- COMMENT THIS OUT
-import HotelSearchWidget from './features/HotelSearchWidget/HotelSearchWidget.jsx';
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */} {/* <-- COMMENT THIS OUT TOO */}
-
-      <main style={{ backgroundColor: '#f2f2f2', minHeight: '100vh', padding: '20px' }}>
-        <HotelSearchWidget />
-      </main>
     </div>
   );
 }
